7cbbcc9817432e77758e7317a618da03a356f520
Nikitha <nikithachimisharma22@gmail.com>
2023-05-03 15:18:14 +0530

    2 files

---
+++ odev.java
@@ -0,0 +1,13 @@
+class oddeven {
+     public static void main(String[] args) {
+    int i=3;
+    if(i%2==0)
+   {
+        System.out.println("Number is even");
+   }
+        else
+        {
+         System.out.println("Number is odd");
+    }
+     System.out.println("Bye!");
+}
